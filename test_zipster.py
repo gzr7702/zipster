295ffbd655ae02ac88b17118c95bed1f7f6a3048
Rob Graziano <robgraziano@earthlink.net>
2012-09-17 18:07:59 -0400

    cleanup

--- test_zipster.py
+++ test_zipster.py
@@ -3,39 +3,40 @@ import os
 import shutil
 import zipfile
 import zipster 
+import subprocess
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        #change this to your test path
-        self.path = "/Users/rob/Documents/workspace/zipster/zip_test"
-        self.zipFilename = "zip_test.zip"
-        os.mkdir(self.path)
-        self.fNames = ["file1", "file2", "file3"]
-        for fn in self.fNames:
-            f = open(os.path.join(self.path, fn), "w")
+        self.test_dir = "zip_test"
+        self.zip_filename = "zip_test.zip"
+        os.mkdir(self.test_dir)
+        self.file_names = ["file1", "file2", "file3"]
+        for fn in self.file_names:
+            f = open(os.path.join(self.test_dir, fn), "w")
             f.write("some data")
             f.close()
     
     def test_zipster(self):
-        #add app name to args becuase that's what will happen from the command line.
-        args = ["zipster.py", self.path]
-        zipster.main(args)
-        #change this to your test path
-        self.newPath = "/Users/rob/Documents/workspace/zipster"
+        #call zipster with arg to dir that should be zipped
+        #obviously, you need to change this to the path on your system or system wide python
+        command = ["/usr/local/bin/python3", "zipster.py", "-s", "zip_test"]
+        subprocess.call(command)
         #handle an unfound zip file gracefully.
+        files_in_archive = None
+        observed = None
         try:
-            zf = zipfile.ZipFile(os.path.join(self.newPath, self.zipFilename), "r")
+            zf = zipfile.ZipFile(self.zip_filename, "r")
             files_in_archive = zf.namelist()
             zf.close()
             observed = set([os.path.basename(f) for f in files_in_archive])
-            expected = set(self.fNames)
-            self.assertEqual(observed, expected)
         except IOError as ioe:
-            print(ioe, "Test failed because a zip file was never created.")
+            print(ioe)
+        expected = set(self.file_names)
+        self.assertEqual(observed, expected)
     
     def tearDown(self):
-        os.remove(self.zipFilename)
-        try:
-            shutil.rmtree(self.path, ignore_errors=True)
-        except IOError:
-            pass
+        shutil.rmtree(self.test_dir, ignore_errors=True)
+        if os.path.exists(self.zip_filename):
+            os.remove(self.zip_filename)
+        else:
+            print("Zip file must not have been created.")
--- zipster.py
+++ zipster.py
@@ -1,38 +1,16 @@
+"""A simple utility that zips all the files in a folder and names the zip file after that folder.
+It takes one argument: the path to the folder that you want to zip.
+"""
 import zipfile
 import glob
 import os
 import sys
-import getopt
+import argparse
+import logging
 
-class Usage(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-        
-def main(argv=None):
-    """A simple utility to zips all the files in a folder and names the zip file after that folder.
-    It takes one argument: the path to the folder that you want to zip.
-    """
-    print("start")
-    if argv is None:
-        argv = sys.argv
-        
-    try:
-        try:
-            opts, args = getopt.getopt(argv[1:], "h", ["help"])
-        except getopt.error as msg:
-            raise Usage(msg)
-    except Usage as err:
-        print >>sys.stderr, err.msg
-        print >>sys.stderr, "for help use --help"
-        return 2
-    
-    for opt in opts:
-        if opt == 'h' or opt == 'help':
-            Usage("zipster.py /path/to/folder")
-            
-    pth = args[0]
-
-    new_path, fn_base = os.path.split(pth)
+def main(args):
+    new_path, fn_base = os.path.split(args.source)
+    print("base", fn_base)
     fn = fn_base + ".zip"
     files_to_archive = glob.glob(os.path.join(fn_base, "*"))
     zf = zipfile.ZipFile(fn, "w", zipfile.ZIP_DEFLATED)
@@ -43,4 +21,8 @@ def main(argv=None):
     return zf.filelist
 
 if __name__ == "__main__":
-    sys.exit(main())
+    parser = argparse.ArgumentParser(description="Zips all files in a directory and names the zip file after the directory")
+    parser.add_argument("-s", "--source", metavar="P", help="The source directory that you would like zipped.")
+    
+    args = parser.parse_args()
+    main(args)
