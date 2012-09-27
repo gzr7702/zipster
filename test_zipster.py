6c37f507c89345aebcdb5d3823efe64c123a6a67
Rob Graziano <robgraziano@earthlink.net>
2012-09-27 19:12:30 -0400

    Added move option

--- test_zipster.py
+++ test_zipster.py
@@ -8,22 +8,25 @@ import subprocess
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.test_dir = "zip_test"
+        self.archive_dir = "archive"
         self.zip_filename = "zip_test.zip"
         os.mkdir(self.test_dir)
+        os.mkdir(self.archive_dir)
         self.file_names = ["file1", "file2", "file3"]
         for fn in self.file_names:
             f = open(os.path.join(self.test_dir, fn), "w")
             f.write("some data")
             f.close()
-    
+            
     def test_zipster(self):
+        "Test basic functionality"
         #call zipster with arg to dir that should be zipped
         #obviously, you need to change this to the path on your system or system wide python
         command = ["/usr/local/bin/python3", "zipster.py", "-s", "zip_test"]
         subprocess.call(command)
-        #handle an unfound zip file gracefully.
         files_in_archive = None
         observed = None
+        #handle an unfound zip file gracefully.
         try:
             zf = zipfile.ZipFile(self.zip_filename, "r")
             files_in_archive = zf.namelist()
@@ -33,10 +36,35 @@ class TestCase(unittest.TestCase):
             print(ioe)
         expected = set(self.file_names)
         self.assertEqual(observed, expected)
-    
+        
+    def test_archive(self):
+        "Test functionality where zip file is moved to archive folder"
+        #call zipster with arg to dir that should be zipped and arg to dir that where zip file will be stored
+        #obviously, you need to change this to the path on your system or system wide python
+        command = ["/usr/local/bin/python3", "zipster.py", "-s", "zip_test", "-d", "archive"]
+        subprocess.call(command)
+        files_in_archive = None
+        observed = None
+        archive_path = os.path.join(self.archive_dir, self.zip_filename)
+        try:
+            print(archive_path)
+            zf = zipfile.ZipFile(archive_path, "r")
+            files_in_archive = zf.namelist()
+            zf.close()
+            observed = set([os.path.basename(f) for f in files_in_archive])
+        except IOError as ioe:
+            print(ioe)
+        expected = set(self.file_names)
+        self.assertEqual(observed, expected)
+        
     def tearDown(self):
         shutil.rmtree(self.test_dir, ignore_errors=True)
+        shutil.rmtree(self.archive_dir, ignore_errors=True)
         if os.path.exists(self.zip_filename):
             os.remove(self.zip_filename)
         else:
             print("Zip file must not have been created.")
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCase)
+    unittest.TextTestRunner(verbosity=2).run(suite)
--- zipster.py
+++ zipster.py
@@ -1,4 +1,7 @@
-"""A simple utility that zips all the files in a folder and names the zip file after that folder.
+#!/usr/bin/env python
+
+"""
+A simple utility that zips all the files in a folder and names the zip file after that folder.
 It takes one argument: the path to the folder that you want to zip.
 """
 import zipfile
@@ -6,26 +9,47 @@ import glob
 import os
 import argparse
 import logging
+import shutil
 
 def zipster(args):
     new_path, fn_base = os.path.split(args.source)
+
+    if not os.path.exists(args.source):
+        print("Directory %s does not exist" % fn_base)
+        logging.error("Directory %s does not exist" % fn_base)
+        exit(1)
+
     logging.info("Zipping file " + fn_base)
     fn = fn_base + ".zip"
     files_to_archive = glob.glob(os.path.join(fn_base, "*"))
     zf = zipfile.ZipFile(fn, "w", zipfile.ZIP_DEFLATED)
+    
     for fn_to_archive in files_to_archive:
         logging.info("Adding file " + fn_to_archive + " to archive " + fn_base)
         if os.path.isfile(fn):
             zf.write(fn_to_archive)
-    logging.info("Done!")
     zf.close()
+            
+    if args.dest:
+        if not os.path.exists(args.dest):
+            print("Directory %s does not exist. Leaving zip file where it is." % fn_base)
+            logging.error("Directory %s does not exist. Leaving zip fle where it is." % fn_base)
+        else:
+            dest = os.path.join(args.dest, fn)
+            logging.info("moving file " + fn + " to " + dest)
+            shutil.move(fn, dest)
+        
+    logging.info("Done!")
+    
     return zf.filelist
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Zips all files in a directory and names the zip file after the directory")
-    parser.add_argument("-s", "--source", metavar="P", help="The source directory that you would like zipped.")
-    
+    parser.add_argument("-s", "--source", metavar="source", required=True,
+                        help="The source directory that you would like zipped.")
+    parser.add_argument("-d", "--dest", metavar="destination", help="If specified, a destination directory that the zip file will be moved to.")
+
     args = parser.parse_args()
     LOG_FORMAT = "%(asctime)s - %(levelname)s - %(funcName)s - %(message)s"
-    logging.basicConfig(filename="zipster.log", level=logging.INFO, format=LOG_FORMAT)
+    logging.basicConfig(filename="zipsterlog.txt", level=logging.INFO, format=LOG_FORMAT)
     zipster(args)
